e3355f3e11d6ba2230dcba237271f36dcb8583fa
Alexander Bayanov <alex@bayanov.org>
2019-03-29 01:47:15 +0700

    Moved main class to another package

--- Shapes/src/me/bayanov/shapesMain/Main.java
+++ Shapes/src/me/bayanov/shapesMain/Main.java
@@ -1,10 +1,10 @@
-package me.bayanov.shapes;
+package me.bayanov.shapesMain;
+
+import me.bayanov.shapes.*;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class Main {
-
     public static void main(String[] args) {
         Shape square1 = new Square(10);
         Shape square2 = new Square(20);
@@ -12,8 +12,8 @@ public class Main {
         Shape rectangle1 = new Rectangle(10, 10);
         Shape rectangle2 = new Rectangle(20, 20);
 
-        Shape triangle1 = new Triangle(10, 20,20,20, 30, 30);
-        Shape triangle2 = new Triangle(10, 10 , 10, 30, 30, 30);
+        Shape triangle1 = new Triangle(10, 20, 20, 20, 30, 30);
+        Shape triangle2 = new Triangle(10, 10, 10, 30, 30, 30);
 
         Shape circle1 = new Circle(10);
         Shape circle2 = new Circle(20);
@@ -21,23 +21,15 @@ public class Main {
         Shape[] array = {square1, square2, rectangle1, rectangle2, triangle1, triangle2, circle1, circle2};
 
 //        Sort by area
-        Arrays.sort(array, new Comparator<Shape>() {
-            @Override
-            public int compare(Shape o1, Shape o2) {
-                return Double.compare(o2.getArea(), o1.getArea());
-            }
-        });
+        ShapeAreaComparator areaComparator = new ShapeAreaComparator();
+        Arrays.sort(array, areaComparator);
 
         System.out.printf("Max area: %f %nIt is %s%n%n",
                 array[0].getArea(), array[0]);
 
 //        Sort by perimeter
-        Arrays.sort(array, new Comparator<Shape>() {
-            @Override
-            public int compare(Shape o1, Shape o2) {
-                return Double.compare(o2.getPerimeter(), o1.getPerimeter());
-            }
-        });
+        ShapePerimeterComparator perimeterComparator = new ShapePerimeterComparator();
+        Arrays.sort(array, perimeterComparator);
 
         System.out.printf("Second perimeter: %f %nIt is %s",
                 array[1].getPerimeter(), array[1]);
