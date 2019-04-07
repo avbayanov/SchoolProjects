7b19206cf370fb67939db6a3ce3f62d29a710e03
Alexander Bayanov <alex@bayanov.org>
2019-04-07 20:27:57 +0700

    Simplified Range.getUnion

--- Range/src/me/bayanov/range/Range.java
+++ Range/src/me/bayanov/range/Range.java
@@ -47,26 +47,13 @@ public class Range {
             throw new NullPointerException("Range must not be null");
         }
 
-        if (range.from >= from) {
-            if (range.to >= to) {
-                if (range.from > to) {
-                    return new Range[]{new Range(from, to), new Range(range.from, range.to)};
-                } else {
-                    return new Range[]{new Range(from, range.to)};
-                }
-            } else {
-                return new Range[]{new Range(from, to)};
-            }
+        if (isInside(range.from) || isInside(range.to) || range.isInside(from) || range.isInside(to)) {
+            return new Range[]{new Range(Math.min(from, range.from), Math.max(to, range.to))};
         } else {
-            if (range.to >= to) {
-                return new Range[]{new Range(range.from, range.to)};
-            } else {
-                if (from > range.to) {
-                    return new Range[]{new Range(range.from, range.to), new Range(from, to)};
-                } else {
-                    return new Range[]{new Range(range.from, to)};
-                }
-            }
+            return new Range[]{
+                    new Range(Math.min(from, range.from), Math.min(to, range.to)),
+                    new Range(Math.max(from, range.from), Math.max(to, range.to))
+            };
         }
     }
 
