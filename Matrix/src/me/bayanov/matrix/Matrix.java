1bfd4b8a1f5c54a9e2fd9fea9ae279e79bdf90cc
Alexander Bayanov <alex@bayanov.org>
2019-04-10 21:06:00 +0700

    Simplified subtract(Matrix)

--- Matrix/src/me/bayanov/matrix/Matrix.java
+++ Matrix/src/me/bayanov/matrix/Matrix.java
@@ -241,11 +241,7 @@ public class Matrix {
         }
 
         for (int i = 0; i < getRowsCount(); i++) {
-            Vector currentRowInMatrix = matrix.getRow(i);
-
-            for (int j = 0; j < getColumnsCount(); j++) {
-                rows[i].setComponent(j, rows[i].getComponent(j) - currentRowInMatrix.getComponent(j));
-            }
+            rows[i].subtract(matrix.getRow(i));
         }
     }
 
