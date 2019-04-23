92e6b37dd06496b5d592f5859da657cdab2bb3ec
Alexander Bayanov <alex@bayanov.org>
2019-04-23 21:01:43 +0700

    Removed redundant import

--- Temperature/src/me/bayanov/temperatureMain/Main.java
+++ Temperature/src/me/bayanov/temperatureMain/Main.java
@@ -4,8 +4,6 @@ import me.bayanov.temperature.Controller;
 import me.bayanov.temperature.Converter;
 import me.bayanov.temperature.View;
 
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller(new Converter(), new View());
