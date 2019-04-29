94aa2010b67be83d139262e5bc05a2036c606d30
Alexander Bayanov <alex@bayanov.org>
2019-04-30 02:43:49 +0700

    Changed literals in constants

--- Temperature/src/me/bayanov/temperature/Converter/FahrenheitConverter.java
+++ Temperature/src/me/bayanov/temperature/Converter/FahrenheitConverter.java
@@ -1,8 +1,8 @@
 package me.bayanov.temperature.Converter;
 
 public class FahrenheitConverter implements Converter {
-    private static final double multiplierTo = 9F / 5;
-    private static final double multiplierFrom = 5F / 9;
+    private static final double multiplierTo = 9d / 5;
+    private static final double multiplierFrom = 5d / 9;
     private static final double difference = 32;
 
     @Override
